06556b49d911c54fba00955f6673cd0a393e08e5
Ahmad Anbar <aanbar@gmail.com>
2018-12-20 04:36:25 +0200

    Add model & controller.

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PlaceOrderRequest;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    public function place_order(PlaceOrderRequest $request)
+    {
+
+    }
+}
